028f4dd989c4a1510a29e93d2c20f1376a23fded
dinhnv <dinhnv.mr@gmail.com>
2016-10-03 17:59:54 +0700

    tmux key binding, style and add django layout

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # @author dinhnv
 
@@ -6,13 +6,14 @@ set -e
 # ask sudo password (timeout to cache 15minutes)
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until the script has finished.                           
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
 # constants
 REPO_ROOT=$(pwd)
-APPS_DIR="~/appsx"
+# I don't want to get stuck in trouble of /opt permissions and .apps to be less conflictable
+APPS_DIR="$HOME/.apps"
 
 
 # mkdir
@@ -30,6 +31,15 @@ su_apt() {
     sudo apt-get install -y $@
 }
 
+# backup if existed
+backup() {
+    if [ -f $1 ] || [ -d $1 ]; then
+        mv $1 "$1.origin"
+    fi
+}
+
+
+# install
 install_os_dependencies() {
     log "install os dependencies"
     # build
@@ -37,9 +47,9 @@ install_os_dependencies() {
     # dev
     su_apt zlib1g-dev libpq-dev libtiff5-dev libjpeg8-dev libfreetype6-dev\
     liblcms2-dev libwebp-dev graphviz-dev gettext libbz2-dev\
-    libreadline-dev libsqlite3-dev
+    libreadline-dev libsqlite3-dev xclip
     # vim
-    su_apt ncurses-dev 
+    su_apt ncurses-dev
     # tmux
     su_apt libevent-dev
 }
@@ -51,26 +61,31 @@ _tmux() {
     sh autogen.sh
     ./configure && make
     sudo make install
+    sudo apt-get install tmuxinator
 }
 
 _neovim() {
     sudo add-apt-repository ppa:neovim-ppa/unstable
     sudo apt-get update
     su_apt neovim
+    # clipboard, search, ctags for plugin requirements
+    su_apt silversearcher-ag ctags
 }
 
 
 link_dotfiles() {
     log "make softlink dotfiles"
-    ln -s "$REPO_ROOT/tmux/tmux.conf" ~/.tmux.conf
-    ln -s "$REPO_ROOT/vim/init.vim" ~/.config/nvim/init.vim
+    tmux_conf="$HOME/.tmux.conf"
+    backup $tmux_conf && ln -s "$REPO_ROOT/tmux/tmux.conf" $tmux_conf
+
+    nv_dir="$HOME/.config/nvim/"
+    mkdir -p $nv_dir && backup "$nv_dir/init.vim"\
+        && ln -s $REPO_ROOT/vim/init.vim "$nv_dir/init.vim"
 }
 
 
 __main__() {
     log "start installing!"
-    # install_os_dependencies
-    _neovim
 }
 
 __main__
--- vim/init.vim
+++ vim/init.vim
@@ -1,1 +1,7 @@
 set termguicolors
+
+" copy/paste
+if has('clipboard')
+    " When possible use + register for copy-paste
+    set clipboard=unnamed,unnamedplus
+endif
