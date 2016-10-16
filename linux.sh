920a30cd16f8a1d8ccac992af8cfedeae0e09ff7
dinhnv <dinhnv.mr@gmail.com>
2016-10-16 19:29:45 +0700

    fix pyenv, rm pyenv-virtualenvwrapper

--- linux.sh
+++ linux.sh
@@ -148,17 +148,16 @@ python_env() {
 
     # pyenv
     curl -L https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-    git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
+    # git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
     append_profile <<EOF
 
 # pyenv
 export PYENV_ROOT="\$HOME/.pyenv"
-export PATH="\$PYENV_ROOT/bin:\$PATH"
-export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
-eval "\$(pyenv init -)"
-eval "\$(pyenv virtualenv-init -)"
-eval "\$(pyenv virtualenvwrapper -)"
-
+if [[ -d \$PYENV_ROOT ]];then
+    PATH="\$PYENV_ROOT/bin:$PATH"
+    eval "\$(pyenv init -)"
+    eval "\$(pyenv virtualenv-init -)"
+fi
 EOF
 
     sudo pip3 install virtualenv
